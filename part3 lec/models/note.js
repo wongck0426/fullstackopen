a0afa622e7c698a57dc8df18a7ed13967b910496
wongck0426 <ken21personal@gmail.com>
2023-03-12 17:06:06 -0700

    console

--- part3 lec/models/note.js
+++ part3 lec/models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+console.log('connecting to-------------', url)
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
