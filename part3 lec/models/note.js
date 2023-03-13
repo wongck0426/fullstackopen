857bce6213fdaeadefb51c426e296dbfca62d4bd
wongck0426 <ken21personal@gmail.com>
2023-03-12 21:15:40 -0700

    change/URI

--- part3 lec/models/note.js
+++ part3 lec/models/note.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-const url = process.env.MONGODB_URI
 
-console.log('connecting to-------------', url)
-mongoose.connect(url)
+
+console.log(`connecting to ${process.env.MONGODB_URI}`)
+mongoose.connect(process.env.MONGODB_URI)
   .then(result => {
     console.log('connected to MongoDB')
   }).catch((error) => {
