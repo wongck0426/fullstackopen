3f52ad90358ebcde9be7ad4f4731bac27128d8d2
wongck0426 <ken21personal@gmail.com>
2023-04-25 23:21:45 -0700

    uibuild

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-console.log('Logging url before connecting', url)
 const url = process.env.MONGODB_URI_PHONEBOOK
+console.log('Logging url before connecting', url)
 
 mongoose.connect(url)
   .then(result => console.log('connected to MongoDB', result))
